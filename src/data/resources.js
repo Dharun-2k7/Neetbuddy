72912ff4a52cbe09cdb880de958aa6d099bf4815
sivaneetbuddy <sivahifi360@gmail.com>
2026-06-25 08:13:05 +0530

    Create src/data/resources.js

---
+++ src/data/resources.js
@@ -0,0 +1,79 @@
+export const physics = [
+  "Units and Dimensions",
+  "Kinematics",
+  "Mechanics: NLM, WPE, Rotational Motion, COM",
+  "Circular Motion",
+  "Basic Maths",
+  "Mechanical Properties of Solids and Fluids",
+  "Thermal Properties",
+  "Thermodynamics",
+  "Kinetic Theory of Gases",
+  "Oscillations and Waves",
+  "Electrostatics",
+  "Current Electricity",
+  "Magnetism and Matter",
+  "Moving Charges and Magnetism",
+  "Electromagnetic Induction",
+  "Alternating Current",
+  "Electromagnetic Waves",
+  "Ray Optics and Wave Optics",
+  "Semiconductors",
+  "Atoms and Nuclei"
+];
+
+export const chemistry = {
+  organic: [
+    "GOC",
+    "Hydrocarbons",
+    "Amines",
+    "Halogen Derivatives",
+    "Carbonyl Compounds",
+    "Alcohols, Phenols and Ethers",
+    "Biomolecules"
+  ],
+  physical: [
+    "Basic Concepts of Chemistry",
+    "Atomic Structure",
+    "Thermodynamics",
+    "Equilibrium",
+    "Chemical Kinetics",
+    "Electrochemistry",
+    "Redox Reactions",
+    "Solutions"
+  ],
+  inorganic: [
+    "Chemical Bonding",
+    "Periodic Table",
+    "P Block",
+    "F Block",
+    "D Block",
+    "Coordination Compounds"
+  ]
+};
+
+export const biology = [
+  "Living Kingdom",
+  "Biological Classification",
+  "Animal Kingdom",
+  "Plant Kingdom",
+  "Plant Physiology",
+  "Human Physiology",
+  "Cell",
+  "Cell Division",
+  "Biomolecules",
+  "Morphology and Anatomy of Flowering Plants",
+  "Structural Organisation in Animals",
+  "Sexual Reproduction in Flowering Plants",
+  "Reproduction in Humans",
+  "Reproductive Health",
+  "Principles of Inheritance and Variation",
+  "Molecular Basis of Inheritance",
+  "Evolution",
+  "Microbes in Human Welfare",
+  "Biotechnology and its Applications",
+  "Biotechnology",
+  "Human Health and Diseases",
+  "Organisms and Population",
+  "Biodiversity and its Conservation",
+  "Ecosystem"
+];
