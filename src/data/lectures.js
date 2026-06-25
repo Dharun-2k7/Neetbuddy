b86f513821285a76cb36390f7ba1ea618e20a5a8
sivaneetbuddy <sivahifi360@gmail.com>
2026-06-25 18:07:43 +0530

    Create src/data/lectures.js

---
+++ src/data/lectures.js
@@ -0,0 +1,14 @@
+export const lectures = {
+  physics: [
+    { lang: "English", teacher: "Physics Wallah", url: "https://www.youtube.com/" },
+    { lang: "English", teacher: "Neso Academy", url: "https://www.youtube.com/" }
+  ],
+  chemistry: [
+    { lang: "English", teacher: "Dexter Chemistry", url: "https://www.youtube.com/" },
+    { lang: "English", teacher: "Unacademy JEE/NEET", url: "https://www.youtube.com/" }
+  ],
+  biology: [
+    { lang: "English", teacher: "Biology at Ease", url: "https://www.youtube.com/" },
+    { lang: "English", teacher: "Aakash BYJU'S Biology", url: "https://www.youtube.com/" }
+  ]
+};
