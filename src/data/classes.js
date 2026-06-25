79f4f42b50cac2fc30853b505e07c1ab5352ca62
sivaneetbuddy <sivahifi360@gmail.com>
2026-06-25 08:12:06 +0530

    Create src/data/classes.js

---
+++ src/data/classes.js
@@ -0,0 +1,7 @@
+export const classes = [
+  "Class 11 - 2 Year Programme",
+  "Class 12 - 1 Year Programme",
+  "Class 12 - 6 Months Programme",
+  "1.5 Year Programme - Repeater / Wasted Year",
+  "Dropper - 1 Year Fast Track",
+];
